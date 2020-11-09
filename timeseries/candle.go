0f71e11afa71c3059dfda840d5b51b0791ac1000
Evgeny Samsonov <bitniks@yandex.ru>
2020-11-09 22:51:45 +0300

    Add validate tag to Candle

--- timeseries/candle.go
+++ timeseries/candle.go
@@ -4,12 +4,12 @@ import "time"
 
 // Candle represents trading candle
 type Candle struct {
-	Time   time.Time `json:"time" db:"time"`
-	High   float64   `json:"high" db:"high"`
-	Low    float64   `json:"low" db:"low"`
-	Open   float64   `json:"open" db:"open"`
-	Close  float64   `json:"close" db:"close"`
-	Volume int64     `json:"volume" db:"volume"`
+	Time   time.Time `json:"time" db:"time" validate:"required"`
+	High   float64   `json:"high" db:"high" validate:"required"`
+	Low    float64   `json:"low" db:"low" validate:"required"`
+	Open   float64   `json:"open" db:"open" validate:"required"`
+	Close  float64   `json:"close" db:"close" validate:"required"`
+	Volume int64     `json:"volume" db:"volume" validate:"required"`
 }
 
 // NewCandle creates new candle
