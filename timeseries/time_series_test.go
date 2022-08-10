5a01bbd7a75093a6a7959a086ba29d02c459939c
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-08-10 23:34:08 +0300

    Update test case correction according to function change

--- timeseries/time_series_test.go
+++ timeseries/time_series_test.go
@@ -65,30 +65,37 @@ func TestTimeSeries_Trim(t *testing.T) {
 	timeSeries := New()
 
 	t.Run("candle=nil", func(t *testing.T) {
-		_, err := timeSeries.Trim(0, nil)
+		_, err := timeSeries.Trim(0, 0)
 		assert.EqualError(t, err, "timeseries cannot be empty")
 	})
 
 	t.Run("startIndex=negative", func(t *testing.T) {
 		err := timeSeries.AddCandle(createTestCandle())
-		_, err1 := timeSeries.Trim(-1, nil)
+		_, err1 := timeSeries.Trim(-1, 0)
 
 		assert.Nil(t, err)
 		assert.EqualError(t, err1, "startIndex cannot be negative")
 	})
 
-	t.Run("endIndex=cannot_bigger_than_startIndex", func(t *testing.T) {
+	t.Run("endIndex=negative", func(t *testing.T) {
 		endIndex := -1
-		_, err1 := timeSeries.Trim(0, &endIndex)
+		_, err1 := timeSeries.Trim(0, endIndex)
+
+		assert.EqualError(t, err1, "endIndex cannot be negative")
+	})
+
+	t.Run("endIndex=negative", func(t *testing.T) {
+		endIndex := 1
+		_, err1 := timeSeries.Trim(1, endIndex)
 
 		assert.EqualError(t, err1, "endIndex should be greater than startIndex")
 	})
 
 	t.Run("endIndex=cannot_bigger_than_timeseries_length", func(t *testing.T) {
 		endIndex := 2
-		_, err1 := timeSeries.Trim(0, &endIndex)
+		_, err1 := timeSeries.Trim(0, endIndex)
 
-		assert.EqualError(t, err1, "endIndex should be less than candle size")
+		assert.EqualError(t, err1, "endIndex should be less than equal to candle size")
 	})
 
 	t.Run("startIndex=single new ts should return", func(t *testing.T) {
@@ -98,7 +105,7 @@ func TestTimeSeries_Trim(t *testing.T) {
 		err := timeSeries.AddCandle(candle)
 
 		assert.Nil(t, err)
-		newTs, _ := timeSeries.Trim(1, nil)
+		newTs, _ := timeSeries.Trim(1, 0)
 
 		assert.Greater(t, timeSeries.Length(), newTs.Length())
 	})
@@ -117,7 +124,7 @@ func TestTimeSeries_Trim(t *testing.T) {
 		assert.Nil(t, err1)
 		assert.Nil(t, err2)
 
-		newTs, _ := timeSeries.Trim(2, &endIndex)
+		newTs, _ := timeSeries.Trim(2, endIndex)
 
 		assert.Greater(t, timeSeries.Length(), newTs.Length())
 	})
