b07ece47642bd70249d4d9720ac8ebee7ae4ed1a
Evgeny Samsonov <bitniks@yandex.ru>
2024-04-03 15:35:04 +0300

    Add IsComplete to candle (#7)
    * Add IsComplete to candle
    
    * Fix test

--- timeseries/candle.go
+++ timeseries/candle.go
@@ -4,12 +4,13 @@ import "time"
 
 // Candle represents trading candle
 type Candle struct {
-	Time   time.Time `json:"time" db:"time" validate:"required"`
-	High   float64   `json:"high" db:"high" validate:"required"`
-	Low    float64   `json:"low" db:"low" validate:"required"`
-	Open   float64   `json:"open" db:"open" validate:"required"`
-	Close  float64   `json:"close" db:"close" validate:"required"`
-	Volume int64     `json:"volume" db:"volume" validate:"required"`
+	Time       time.Time `json:"time" db:"time" validate:"required"`
+	High       float64   `json:"high" db:"high" validate:"required"`
+	Low        float64   `json:"low" db:"low" validate:"required"`
+	Open       float64   `json:"open" db:"open" validate:"required"`
+	Close      float64   `json:"close" db:"close" validate:"required"`
+	Volume     int64     `json:"volume" db:"volume" validate:"required"`
+	IsComplete bool      `json:"is_complete" db:"is_complete"`
 }
 
 // NewCandle creates new candle
--- timeseries/time_series.go
+++ timeseries/time_series.go
@@ -70,7 +70,7 @@ func (ts *TimeSeries) Trim(startIndex int, endIndex int) (*TimeSeries, error) {
 	return &newTS, nil
 }
 
-// LastCandle returns last candle in series
+// LastCandle returns last candle in series or nil if series is empty
 func (ts *TimeSeries) LastCandle() *Candle {
 	if len(ts.candles) > 0 {
 		return ts.candles[len(ts.candles)-1]
@@ -79,7 +79,19 @@ func (ts *TimeSeries) LastCandle() *Candle {
 	return nil
 }
 
-// Candle returns candle by index [0, len(series)-1]
+// LastCompleteCandle returns last complete candle and index in series or nil if there are no complete candles
+func (ts *TimeSeries) LastCompleteCandle() (*Candle, int) {
+	for i := ts.Length() - 1; i >= 0; i-- {
+		candle := ts.Candle(i)
+		if candle.IsComplete {
+			return candle, i
+		}
+	}
+
+	return nil, 0
+}
+
+// Candle returns candle by index [0, len(series)-1] or nil if index out of series
 func (ts *TimeSeries) Candle(index int) *Candle {
 	if index >= 0 && index < len(ts.candles) {
 		return ts.candles[index]
